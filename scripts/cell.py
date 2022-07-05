1fd1baf31a352bbc6db7a2c493d412cf75caca75
Morgan.DeGregorio <Morgan.DeGregorio@opal-rt.com>
2022-07-05 11:40:30 -0400

    Final version of distances

--- scripts/cell.py
+++ scripts/cell.py
@@ -1,14 +1,16 @@
 from enum import Enum
 
-class Color(Enum):
-    RED = 1
-    WHITE = 2
+class Family(Enum):
+    MICROGLIA = 1
+    PNN = 2
+    
     
 class Cell:
-    def __init__(self, id, position_x, position_y, type: Color):
+    def __init__(self, id, position_x, position_y, family: Family, type):
         self.id = id
         self.position_x = position_x
         self.position_y = position_y
+        self.family = family
         self.type = type
     
     
--- scripts/generate_distances.py
+++ scripts/generate_distances.py
@@ -1,55 +1,100 @@
 from math import dist
 import openpyxl
 from cell import Cell
-from cell import Color
+from cell import Family
+import pandas as pd 
 
 PATH_DATA = "Neuroscience_data_analysis\data\PNN_Microglia_distance.xlsx"
 SHEET = 's512 B2 centered (1) detection '
+
 wb = openpyxl.load_workbook(PATH_DATA)
+
+def generate_distances(worksheet_name, sorted_cells, type):
+    sheet = wb[worksheet_name]
+    wb.active = sheet
+    micro_list = sorted_cells[type]['microglia_cells_list']
+    pnn_list = sorted_cells[type]['pnn_cells_list']
+    # Naming headers
+    col=2
+    for microglia_cell in micro_list:
+        sheet.cell(row=1,column=col).value=microglia_cell.id
+        col += 1
+    row=2
+    for pnn_cell in pnn_list:
+        sheet.cell(row=row,column=1).value=pnn_cell.id
+        row += 1
+
+    distances = []
+    # Calulating distances
+    for  r_index, microglia_cell in enumerate(micro_list):
+        for  w_index, pnn_cell in enumerate(pnn_list):
+            point_microglia = [microglia_cell.position_x, microglia_cell.position_y]
+            point_pnn = [pnn_cell.position_x, pnn_cell.position_y]
+            distance = dist(point_microglia, point_pnn)
+            sheet.cell(row=w_index+2, column=r_index+2).value=distance
+            distances.append(distance)
+    return pd.Series(data=distances)
+            
 sheet1 = wb[SHEET]
 wb.active = sheet1
 row_count = sheet1.max_row
 col_count = sheet1.max_column
 
-print(row_count)
-print(col_count)
-
+sorted_cells = {
+    'L1_2' : {
+        'microglia_cells_list' : [],
+        'pnn_cells_list' : []
+        },
+    'L3' : {
+        'microglia_cells_list' : [],
+        'pnn_cells_list' : []
+        },
+    'L4' : {
+        'microglia_cells_list' : [],
+        'pnn_cells_list' : []
+        },
+    'L5' : {
+        'microglia_cells_list' : [],
+        'pnn_cells_list' : []
+        },
+    'L6' : {
+        'microglia_cells_list' : [],
+        'pnn_cells_list' : []
+        }
+}
 # Initializing Cells
-red_cells_list = []
-white_cells_list = []
 for i in range(2, row_count + 1):
-    curr_id = "Cell #" + str(i)
-    curr_type = Color.WHITE
-    if "PNN" in sheet1.cell(row=i, column=1).value:
-        curr_type = Color.RED
+    curr_name = sheet1.cell(row=i, column=1).value
+    curr_type = curr_name.split(' - ')[1]
+    curr_family = Family.MICROGLIA
+    if "PNN" in curr_name:
+        curr_family = Family.PNN
     curr_x = sheet1.cell(row=i, column=2).value
     curr_y = sheet1.cell(row=i, column=3).value
-    curr_cell = Cell(curr_id,curr_x,curr_y,curr_type)
-    if curr_cell.type == Color.WHITE:
-        red_cells_list.append(curr_cell)
+    if curr_family == Family.MICROGLIA:
+        curr_id = "Microglia #" + str(i)
+        curr_cell = Cell(curr_id, curr_x, curr_y, curr_family, curr_type)
+        sorted_cells[curr_type]['microglia_cells_list'].append(curr_cell)
     else:
-        white_cells_list.append(curr_cell)
+        curr_id = "PNN #" + str(i)
+        curr_cell = Cell(curr_id, curr_x, curr_y, curr_family, curr_type)
+        sorted_cells[curr_type]['pnn_cells_list'].append(curr_cell)
 
-sheet2 = wb['Distances']
-wb.active = sheet2
+# Sort seriess
+    
+L1_series = generate_distances('Distances L1_2', sorted_cells, 'L1_2')
+L3_series = generate_distances('Distances L3', sorted_cells, 'L3')
+L4_series = generate_distances('Distances L4', sorted_cells, 'L4')
+L5_series = generate_distances('Distances L5', sorted_cells, 'L5')
+L6_series = generate_distances('Distances L6', sorted_cells, 'L6')
 
-# Naming headers
-col=2
-for red_cell in red_cells_list:
-    sheet2.cell(row=1,column=col).value=red_cell.id
-    col += 1
-row=2
-for white_cell in white_cells_list:
-    sheet2.cell(row=row,column=1).value=white_cell.id
-    row += 1
+# Data analysis
+print("----------\nL1_2\nAverage: " + str(L1_series.mean()) + "\nMedian: " + str(L1_series.median()) + "\nStandard deviation: " + str(L1_series.std()))
+print("----------\nL3\nAverage: " + str(L3_series.mean()) + "\nMedian: " + str(L3_series.median()) + "\nStandard deviation: " + str(L3_series.std()))
+print("----------\nL4\nAverage: " + str(L4_series.mean()) + "\nMedian: " + str(L4_series.median()) + "\nStandard deviation: " + str(L4_series.std()))
+print("----------\nL5\nAverage: " + str(L5_series.mean()) + "\nMedian: " + str(L5_series.median()) + "\nStandard deviation: " + str(L5_series.std()))
+print("----------\nL6\nAverage: " + str(L6_series.mean()) + "\nMedian: " + str(L6_series.median()) + "\nStandard deviation: " + str(L6_series.std()))
 
-# Calulating distances
-for  r_index, red_cell in enumerate(red_cells_list):
-    for  w_index, white_cell in enumerate(white_cells_list):
-        point_red = [red_cell.position_x, red_cell.position_y]
-        point_white = [white_cell.position_x, white_cell.position_y]
-        distance = dist(point_red, point_white)
-        sheet2.cell(row=r_index+2, column=w_index+2).value=distance
 wb.save(PATH_DATA)
 
     
